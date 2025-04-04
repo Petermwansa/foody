f16e54e6d2b0c98436acd41d5e093145ec8deb63
Petermwansa <mwansapeter800@gmail.com>
2025-04-04 23:59:26 +0300

    Add the navbar, the header and the categories sections

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-contents'>
+        <h2>Order your favourite food with Foody</h2>
+        <p>We have a diverse menu for all your tastes</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Placeorder.css';
+
+
+const PlaceOrder = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PlaceOrder
